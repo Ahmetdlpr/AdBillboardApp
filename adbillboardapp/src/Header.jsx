ed0db13e9f1c6c08d1ff337478abbb24d4ae5a9d
Ahmetdlpr <mybqadresadrs@outlook.com>
2024-11-03 13:52:29 +0300

    header componenti ve css eklendi  bazı dosyalar güncellendi

---
+++ adbillboardapp/src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <div >
+        <header className="header">
+        <h2>Eğitim Yolu</h2>
+        <p>İster kurum binamızda ister online olarak eğitim programlarımızı seçebilirsiniz</p>
+       
+        </header>      
+    </div>
+  )
+}
+
+export default Header
